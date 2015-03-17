59446b404af285b869e9e7b9b3675fad1d1bd075
ethornbury <ethornbury@eircom.net>
2015-03-17 14:29:20 +0000

    user set up, tests, name and email confirm, validate, secure password

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+    #validates(:name, presence: true)
+    validates :name,  presence: true, length: { maximum: 80 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+   has_secure_password  #facilitates a securely saved password with pword confirmation, and authenticates user
+   # the above needs password digest
+   validates :password, length: { minimum: 6 }
+end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | HR App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | HR App"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Get in Contact | HR App"
   end
 
 end
