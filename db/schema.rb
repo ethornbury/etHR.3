3d5d4a5192252fe051f0a4de81f0630e00339835
ethornbury <ethornbury@eircom.net>
2015-03-18 15:34:15 +0000

    finish sign in/out. save session, remember me on computer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317142358) do
+ActiveRecord::Schema.define(version: 20150318131520) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150317142358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,4 +26,36 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+  
+   test "login with valid information followed by logout" do
+    get signin_path
+    post signin_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", signin_path, count: 0
+    assert_select "a[href=?]", signout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete signout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete signout_path
+    follow_redirect!
+    assert_select "a[href=?]", signin_path
+    assert_select "a[href=?]", signout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  
+   test "login with remembering" do
+    sign_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    sign_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+  
 end
